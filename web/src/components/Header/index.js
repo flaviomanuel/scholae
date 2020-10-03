6f7a753bd99b191158f398a0dbc5f98c1f0d763e
unknown <flaviomanuel258@gmail.com>
2020-10-03 19:08:23 -0300

    making header on ListPage

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/scholaeYellow.png';
+
+import './styles.css'
+
+function Header() {
+    return (
+        <div>
+            header component
+        </div>
+    )
+}
+
+export default Header; 
--- web/src/pages/ListPage/index.js
+++ web/src/pages/ListPage/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import logoImg from '../../assets/images/scholaeYellow.png';
+
+import './styles.css'
+
 function ListPage(){
     return (
-        <div>
-            List Page Content
+        <div id="list-page">
+            <div className="container" id="list-page-content">
+                <div className="header">
+                    <div className="header-content">
+
+                        <img src={logoImg} alt="Logo scholae"/>
+
+                        <div className="header-menu">
+                            <a href="/">
+                                Opções 
+                            </a>
+                        </div>
+                    </div>
+                 </div>
+            </div>
+
         </div>
     )
 }
