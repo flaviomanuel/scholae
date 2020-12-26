f22df96046d3d40e637e9d9994de80e177e1e63c
flaviomanuel <flaviomanuel258@gmail.com>
2020-12-26 14:51:33 -0300

    add cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes')
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use(cors());
 
 
 
